86525c0e74c955041da696b603c93665b2bb3942
ivans <ivans@LAPTOP-6O43JKHC>
2024-01-06 03:34:45 +0200

    555

--- src/demo/TestDemo.java
+++ src/demo/TestDemo.java
@@ -2,17 +2,11 @@ package demo;
 
 public class TestDemo {
 
-	
-	//Ivan Samoilenko
-	//Ivan Samoilenko 2
 	public static void main(String[] args) {
-		
+
 		System.out.println("Java is fun");
 
 		System.out.println("GitHub is fun");
 	}
 
-	//This is code from github
-// this is ffffffff
-	
 }
